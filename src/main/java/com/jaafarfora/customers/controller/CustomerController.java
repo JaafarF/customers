d7f61140409264cbe616e81c7a96e61490b5ba65
Jaafar Fora <jaafarfora@gmail.com>
2025-01-19 19:52:32 +0100

    test1

--- src/main/java/com/jaafarfora/customers/controller/CustomerController.java
+++ src/main/java/com/jaafarfora/customers/controller/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/customers")
+@RequestMapping("customers")
 public class CustomerController {
 
   private final CustomerServiceImpl customerService;
